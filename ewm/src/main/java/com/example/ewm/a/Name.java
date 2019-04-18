8b56f159d897ed98a5950844f2e61a4605170cc1
demo_Home <1500720570@qq.com>
2019-04-18 15:32:12 +0800

    11

---
+++ ewm/src/main/java/com/example/ewm/a/Name.java
@@ -0,0 +1,30 @@
+package com.example.ewm.a;
+
+public class Name {
+    private String name;
+    private String password;
+
+    public Name() {
+    }
+
+    public Name(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
